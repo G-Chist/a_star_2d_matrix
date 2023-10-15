29cf233c59f864e8555aa7fdc4bc65c8aaa274b1
G-Chist <42654606+G-Chist@users.noreply.github.com>
2023-10-15 17:18:08 -0400

    Update a_star_visual.py

--- a_star_visual.py
+++ a_star_visual.py
@@ -10,6 +10,9 @@ def find_element(arr, target):
                 return [row, col]
     return None  # Return None if the element is not found
 
+def sum_arrays(a, b):
+    return [a[i] + b[i] for i in range(len(a))]
+
 
 def in_matrix(matrix, row, col):
     num_rows = len(matrix)
@@ -41,8 +44,9 @@ def search_path(matrix):
         for col in range(len(matrix[0])):
             points.append([row, col])
 
+    #One index in last_points, path_costs and points corresponds to the same point
     last_points = [[0, 0] for point in points]
-    path_costs = [float('inf') for point in points]
+    path_costs = [float('inf') if point != start_point else 0 for point in points]
 
     """
     print(f"Points array:      {points}")
@@ -56,13 +60,7 @@ def search_path(matrix):
         return end_cost
 
     def step_cost(point1, point2):
-        if point1[0] == point2[0]:
-            if point1[1] == point2[1]:
-                return 0
-        if point1[0] == point2[0] or point1[1] == point2[1]:
-            return 1
-        else:
-            return math.sqrt(2)
+        return distance(point1[0], point2[0], point1[1], point2[1])
 
     def neighbours_available(point):
         neighbours = []
@@ -71,7 +69,7 @@ def search_path(matrix):
             d_col = point[1] + direction[1]
             if in_matrix(matrix, d_row, d_col):
                 if matrix[d_row][d_col] != BLACK:
-                    if abs(direction[0]) == abs(direction[1]): # Cannot pass between two diagonal obstacles
+                    if abs(direction[0]) == abs(direction[1]): #Cannot pass between two diagonal obstacles
                         if matrix[point[0]][d_col] != BLACK and matrix[d_row][point[1]] != BLACK:
                             neighbours.append([d_row, d_col])
                     else:
@@ -81,42 +79,43 @@ def search_path(matrix):
     def search():
         global chosen_point
 
-        #Stop if end_point is in closed_stack
-        if end_point in closed_stack:
-            return 0
+        #Index of chosen_point in points
+        chosen_index = points.index(chosen_point)
 
-        if chosen_point not in closed_stack:
-            closed_stack.append(chosen_point)
-
+        #Check available neighbours
         neighbours = neighbours_available(chosen_point)
 
-        #Put neighbours in open_stack
-        for n in neighbours:
-            if n not in closed_stack:
-                if n not in open_stack:
-                    open_stack.append(n)
+        #Update neighbours' costs and last points
+        for neighbour in neighbours:
+            if neighbour not in open_stack and neighbour not in closed_stack:
+                open_stack.append(neighbour)
+            if neighbour not in closed_stack:
+                n_index = points.index(neighbour) #Index of neighbour in points
+                n_last_index = n_index #Index of neighbour's last point in last_points
+                cost = step_cost(chosen_point, neighbour) + path_costs[chosen_index] #Cost to get to neighbour
 
-        #Update last point and path cost
-        for p in points:
-            if p in neighbours and p not in closed_stack:
-                p_index = points.index(p)
-                cost = step_cost(chosen_point, p) + h_cost(p)
-                if cost < path_costs[p_index]:
-                    path_costs[p_index] = cost
-                    last_points[p_index] = chosen_point
+                #Update cost and last point if a better path is found
+                if cost < path_costs[n_index]:
+                    path_costs[n_index] = cost
+                    last_points[n_last_index] = chosen_point
 
-        #Select next point to explore (lowest cost)
-        min_cost_available = float('inf')
-        for p in open_stack:
-            if p not in closed_stack:
-                p_index = points.index(p)
-                if path_costs[p_index] < min_cost_available:
-                    min_cost_available = path_costs[p_index]
-        for p in open_stack:
-            if p not in closed_stack:
-                p_index = points.index(p)
-                if path_costs[p_index] == min_cost_available:
-                    chosen_point = p
+        #Put chosen point in closed stack
+        closed_stack.append(chosen_point)
+
+        #Choose new point
+        full_costs = sum_arrays(path_costs, [h_cost(point) for point in points])
+        min_cost = float('inf')
+        for point in open_stack:
+            if point not in closed_stack:
+                p_index = points.index(point) #Index of point in points
+                if full_costs[p_index] < min_cost:
+                    min_cost = full_costs[p_index]
+
+        for point in open_stack:
+            if point not in closed_stack:
+                p_index = points.index(point)  # Index of point in points
+                if full_costs[p_index] == min_cost:
+                    chosen_point = points[p_index]
 
         """
         print(f"Open stack:        {open_stack}")
@@ -143,7 +142,7 @@ def search_path(matrix):
         screen.fill(GRAY)
         draw_buttons()
         pygame.display.flip()
-        time.sleep(0.005)
+        #time.sleep(0.005)
 
     path = []
     last_point = end_point
@@ -306,4 +305,4 @@ while running:
 pygame.quit()
 sys.exit()
 
-#BAD COST ESTIMATION, FOLLOWS OBSTACLE "WALLS"
+#BAD COST ESTIMATION, FOLLOWS OBSTACLE "WALLS", DOES NOT TAKE DIJKSTRA COST INTO ACCOUNT
